96cc0da21ccc850c741153811f60654fabdca4bc
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-11-06 16:21:01 +0600

    Routes

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_journey/views/home/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Splash Screen"),),);
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("Splash Screen", style: TextStyle(fontSize: 30)),
+          ),
+          SizedBox(height: 50),
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
+          }, child: Text("Go to Home Screen"))
+        ],
+      ),
+    );
   }
-}
+ }
